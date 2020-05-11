14bca475d6852b9e7a6b910cfb697185a62c1c37
Victor Maestro <avmestro19@gmail.com>
2020-05-11 22:00:46 +0200

    cambios en metricas de ev.

--- Extraccion/valoracion_AWS_WP.py
+++ Extraccion/valoracion_AWS_WP.py
@@ -5,6 +5,9 @@ import warnings
 
 warnings.filterwarnings(action='ignore')
 
+def list_diff(list1, list2):
+    return(list(set(list1) - set(list2)))
+
 premios = pd.read_csv("Extraccion/Premios_10_05_20/Premios_10_05_20.csv")
 AWS =  pd.read_csv("Script_seleccion_jugadores/AWS_TOP30.csv")
 WP =  pd.read_csv("Script_seleccion_jugadores/WP_TOP30.csv")
@@ -17,29 +20,29 @@ for season in seasons:
     maximo = len(df_Premios)
     df_Premios = df_Premios.head(maximo)
     df_Premios["Posicion"] = np.arange(1,maximo + 1,1)
-    df_Premios = df_Premios[["Posicion", "nombre", "año"]]
+    df_Premios = df_Premios[["Posicion", "nombre", "año", "primero"]]
     #"nombre": "premios"
     df_Premios.rename(columns = {"nombre": "Premios", "año": "Season"}, inplace = True)
 
     df_AWS = AWS[AWS["Season"] == season ]
     df_AWS = df_AWS.sort_values(by=['AWS_MEAN'], ascending = False)
-    df_AWS = df_AWS.head(maximo)
-    df_AWS["Posicion"] = np.arange(1,maximo + 1,1)
+    df_AWS = df_AWS.head(30)
+    df_AWS["Posicion"] = np.arange(1,30 + 1,1)
     df_AWS = df_AWS[["Posicion", "Name"]]
     #"Name": "AWS"
     df_AWS.rename(columns = {"Name": "AWS"}, inplace = True)
 
-    df_joined = df_Premios.merge(df_AWS, on = "Posicion", how = "left")
+    df_joined = df_Premios.merge(df_AWS, on = "Posicion", how = "right")
     #df_joined['Premios-AWS'] = np.where(df_joined['Premios'] == df_joined['AWS'], True, False)
 
     df_WP = WP[WP["Season"] == season ]
     df_WP = df_WP.sort_values(by=['WP_MEAN'], ascending = False)
-    df_WP = df_WP.head(maximo)
-    df_WP["Posicion"] = np.arange(1,maximo + 1,1)
+    df_WP = df_WP.head(30)
+    df_WP["Posicion"] = np.arange(1,30 + 1,1)
     df_WP = df_WP[["Posicion", "Name"]]
     #"Name": "WP"
     df_WP.rename(columns = {"Name": "WP"}, inplace = True)
-    df_joined = df_joined.merge(df_WP, on = "Posicion", how = "left")
+    df_joined = df_joined.merge(df_WP, on = "Posicion", how = "right")
 
     df_Defensivo = premios[(premios["año"] == season) & (premios["award"] == "Defensive Player of the Year")]
     df_Defensivo = df_Defensivo.sort_values(by=['puntos'], ascending = False)
@@ -53,8 +56,8 @@ for season in seasons:
 
     df_sueldos = sueldos[sueldos["Temporada"] == season]
     df_sueldos = df_sueldos.sort_values(by=['Sueldo'], ascending = False)
-    df_sueldos = df_sueldos.head(maximo)
-    df_sueldos["Posicion"] = np.arange(1,maximo + 1,1)
+    df_sueldos = df_sueldos.head(30)
+    df_sueldos["Posicion"] = np.arange(1,30 + 1,1)
     df_sueldos = df_sueldos[["Posicion", "Nombre"]]
     df_sueldos["Nombre"] = df_sueldos["Nombre"].str.split().str[1] + "," + df_sueldos["Nombre"].str.split().str[0]
     df_sueldos.rename(columns = {"Nombre": "Sueldos"}, inplace = True)
@@ -69,7 +72,7 @@ for season in seasons:
             aux_AWS = aux_AWS + 1 
         if(jugador in df_joined["WP"].tolist()):
             aux_WP = aux_WP + 1
-
+    print("###########", season ,"###########")
     print("AWS", aux_AWS, "aciertos de", maximo, "respecto a premio MVP")
     print("WP", aux_WP, "aciertos de", maximo, "respecto a premio MVP")
 
@@ -83,8 +86,8 @@ for season in seasons:
         if(jugador in df_joined["WP"].tolist()):
             aux_WP = aux_WP + 1
 
-    print("AWS", aux_AWS, "aciertos de", maximo, "respecto a sueldos")
-    print("WP", aux_WP, "aciertos de", maximo, "respecto a sueldos")
+    print("AWS", aux_AWS, "aciertos de", 30, "respecto a sueldos")
+    print("WP", aux_WP, "aciertos de", 30, "respecto a sueldos")
 
     aux_AWS = 0
     aux_WP = 0
@@ -99,6 +102,41 @@ for season in seasons:
     print("AWS", aux_AWS, "aciertos de", maximo, "respecto a premio Defensivo")
     print("WP", aux_WP, "aciertos de", maximo, "respecto a premio Defensivo")
 
+    print("######## AWS ############")
+    print("Votados para el MVP que no aparecen en AWS")
+    print("\t", list_diff(df_joined["Premios"].tolist(), df_joined["AWS"].tolist()))
+    diff_AWS = list_diff(df_joined["Premios"].tolist(), df_joined["AWS"].tolist())
+    puntos_faltantes_primero = 0
+    puntos_faltantes = 0
+    for jugador in diff_AWS:
+        #print(jugador,  int(df_joined[df_joined["Premios"] == jugador]["puntos"]))
+        try:
+            puntos_faltantes_primero = puntos_faltantes + int(df_joined[df_joined["Premios"] == jugador]["primero"])
+            puntos_faltantes = puntos_faltantes + int(df_joined[df_joined["Premios"] == jugador]["Posicion"])
+        except:
+            puntos_faltantes_primero = puntos_faltantes_primero
+            puntos_faltantes = puntos_faltantes
+    print("\t", "Puntos faltantes (votado en primer puesto):", puntos_faltantes_primero)
+    print("\t", "Puntos faltantes (suma de las posiciones no presentes):", puntos_faltantes)
+
+    print("######## WP ############")
+    print("Votados para el MVP que no aparecen en WP")
+    print("\t", list_diff(df_joined["Premios"].tolist(), df_joined["WP"].tolist()))
+    diff_WP = list_diff(df_joined["Premios"].tolist(), df_joined["WP"].tolist())
+    puntos_faltantes_primero = 0
+    puntos_faltantes = 0
+    for jugador in diff_WP:
+        #print(jugador,  int(df_joined[df_joined["Premios"] == jugador]["puntos"]))
+        try:
+            puntos_faltantes_primero = puntos_faltantes_primero + int(df_joined[df_joined["Premios"] == jugador]["primero"])
+            puntos_faltantes = puntos_faltantes + int(df_joined[df_joined["Premios"] == jugador]["Posicion"])
+        except:
+            puntos_faltantes_primero = puntos_faltantes_primero
+            puntos_faltantes = puntos_faltantes
+    print("\t", "Puntos faltantes (votado en primer puesto):", puntos_faltantes_primero)
+    print("\t", "Puntos faltantes (suma de las posiciones no presentes):", puntos_faltantes)
+
+
     li.append(df_joined)
 
 df_final = pd.concat(li, axis=0, ignore_index=True)
@@ -111,4 +149,7 @@ df_final.to_csv("valoracion_AWS_WP.csv", header=True, index=False)
 # - Sueldo
 # Después itera sobre los jugadores de la lista de premios, si el jugador está en la lista de AWS / WP suma 1 a su score
 # Lo mismo para el sueldo. 
-# Tengo la sensación de que WP tienen un cierto sesgo hacia los jugadores ofensivos frente a los defensivos (opinión personal)
+# Tengo la sensación de que WP tienen un cierto sesgo hacia los jugadores ofensivos frente a los defensivos (opinión personal), 
+# pero la escogería por dos motivos:
+# a) siendo las dos listas que salen buenas, creo que es mejor la de WP 
+# b) más allá de la opinión, contempla más datos que el AWS.
